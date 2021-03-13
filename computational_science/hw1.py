1567faf010d647416c5010b56e0766fbfd435040
zendo <p_geondo@snu.ac.kr>
2021-03-14 01:07:18 +0900

    feat: computational science hw 1
    Type B; print some paragraph about complex number.

---
+++ computational_science/hw1.py
@@ -0,0 +1,11 @@
+#############################
+#                           #
+#     계산과학이론 및 실습1    #
+#            HW 1           #
+#      2017-11362 박건도     #
+#                           #
+#############################
+
+print(
+    "class complex([real[, imag]])\nReturn a complex number with the value real + imag*1j or convert a string or number to a complex number. If the first parameter is a string, it will be interpreted as a complex number and the function must be called without a second parameter. The second parameter can never be a string. Each argument may be any numeric type (including complex). If imag is omitted, it defaults to zero and the constructor serves as a numeric conversion like int and float. If both arguments are omitted, returns 0j."
+)
