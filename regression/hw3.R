d9b8b3031452d4c78ed86ff2d3250841ceb9def4
zendo <p_geondo@snu.ac.kr>
2021-03-20 00:03:16 +0900

    fix: regression lab hw3

--- regression/hw3.R
+++ regression/hw3.R
@@ -1,12 +1,24 @@
 library(ggplot2)
 # Q.1
 
-ggplot(data = iris, aes(x = Sepal.Length, y = Sepal.Width)) +
-  geom_point(aes(color=Species)) +
+ggplot(data = iris, aes(x = Sepal.Length, y = Sepal.Width, color=Species)) +
+  geom_point() +
   theme_bw()
 
 
 # Q.2
-
+#(a)
 model = lm(Sepal.Length ~ Petal.Length, data = iris)
-model$coefficients[2]
+summary(model)
+
+#(b)
+my_lm <- function(x, y) {
+  mu = mean(x)
+  beta_1 = sum((x - mu) * y) / sum((x - mu)^2)
+  beta_0 = mean(y - beta_1 * mu)
+  coefs = c(beta_0, beta_1)
+  names(coefs) = c('beta_0', 'beta_1')
+  return(coefs)
+}
+
+my_lm(iris$Petal.Length, iris$Sepal.Length)
