66546737bd0644f42eaffab2d09c59a1475e9f06
zendo <p_geondo@snu.ac.kr>
2021-04-09 21:36:39 +0900

    feat: computational science hw5

--- computational_science/hw5.py
+++ computational_science/hw5.py
@@ -9,7 +9,7 @@
 
 # problem_A
 
-from math import pi, exp
+from math import pi, exp, log
 
 
 def main():
@@ -49,5 +49,40 @@ def adp_simp(a, b, tol=1e-7):
     return sum(area)
 
 
+# problem_B
+
+
+def integral(coefs, interval, n):
+    assert n > 0, "n should be positive integer."
+    a = len(coefs)
+    if coefs[0] != 0:
+        f = lambda x: sum([coefs[a - i - 1] * x ** i for i in range(a - 1, -1, -1)])
+        F = lambda x: sum([coefs[a - i] * x ** (i) / i for i in range(a, 0, -1)])
+    else:
+        assert interval[0] * interval[1] > 0, "0 should not be contained at the interval."
+        f = lambda x: sum([coefs[i] / x ** i for i in range(1, a)])
+        F = lambda x: coefs[1] * log(x) + sum([-coefs[i + 1] / x ** i / i for i in range(1, a - 1)])
+
+    real = F(interval[1]) - F(interval[0])
+    err = lambda e: abs(e - real) / real * 100
+
+    h = (interval[1] - interval[0]) / n
+
+    points = [interval[0] + h * (2 * i + 1) / 2 for i in range(n)]
+    midpoint = sum([f(x) for x in points]) * h
+
+    points = [interval[0] + h * i for i in range(1, n)]
+    trapezoid = (sum([f(x) for x in points]) + (f(interval[0]) + f(interval[1])) / 2) * h
+
+    print("%10s | %10s | %5s(%%)" % ("method", "integral", "error"))
+    print("─" * 34)
+    print("%10s | %10.5f | %5.2f" % ("midpoint", midpoint, err(midpoint)))
+    print("%10s | %10.5f | %5.2f" % ("trapezoid", trapezoid, err(trapezoid)))
+
+    return real
+
+
 if __name__ == "__main__":
-    main()
+    # main()
+    integral([0, 2, 3], (1, 2), 2)
+
